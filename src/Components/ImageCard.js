805c6825e55ceaf2e5c1c91312d7cb845916a9bb
Mustafa Ali <alimustafa0987@gmail.com>
2020-12-29 00:34:00 -0500

    Added image card

---
+++ src/Components/ImageCard.js
@@ -0,0 +1,63 @@
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button'
+import './ImageCard.css'
+
+const useStyles = makeStyles(() => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  }
+}));
+
+const ImageCard = props => {
+
+	const classes = useStyles();
+	const avatarStyle = {backgroundColor: props.imageData.color}
+   
+    return (
+        <Card className={classes.root + " imageCard"}>
+            <CardHeader
+                avatar={
+                <Avatar aria-label="recipe" style={avatarStyle}>
+                    {props.imageData.avatarInitial}
+                </Avatar>
+                }
+                title={props.imageData.title}
+                subheader={props.imageData.date}
+            />
+            <CardMedia
+                className={classes.media}
+                image={props.imageData.imageSrc}
+                title={props.imageData.imageId}
+            />
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+					{props.imageData.description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small" color="primary">
+                	Update
+                </Button>
+                <Button size="small" color="primary">
+                	Delete
+                </Button>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default ImageCard;
--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,12 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import {Auth, Storage} from 'aws-amplify'
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import ImageCard from './ImageCard'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,12 +21,27 @@ const useStyles = makeStyles((theme) => ({
 
 const MainPage = props => {
     const classes = useStyles();
+    const [imagesData, setImagesData] = useState([])
 
     useEffect(() => {
         if (!props.authData.authenticated) {
             props.history.push("/login")
         }
     }, [props.authData.authenticated])
+
+    useEffect(() => {
+        // on initial load, get all images from s3
+        let imageData = {
+            "avatarInitial": "M",
+            "title": "Random Moment in Time",
+            "date": "September 1 2001",
+            "imageSrc": "",
+            "description": "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
+            "color": "green",
+            "imageId": "asfe343e24233 t4"
+        }
+        setImagesData([imageData, imageData, imageData, imageData])
+    }, [])
 
     const onChange = (e) => {
         const file = e.target.files[0];
@@ -37,6 +51,18 @@ const MainPage = props => {
         })
         .then (result => console.log(result))
         .catch(err => console.log(err));
+    }
+
+    const deleteImage = id => {
+        //perform s3 deletion
+    }
+
+    const updateImg = imageData => {
+        //open modal and allow for image updating
+    }
+
+    const createNewImage = imageData => {
+        //perform neessary steps
     }
 
     return (
@@ -53,11 +79,21 @@ const MainPage = props => {
                 }}>Logout</Button>
                 </Toolbar>
             </AppBar>
-            <h1>Hey {props.authData.user != null ? props.authData.user.username : "none"}</h1>
-            <input
-              type="file" accept='image/png'
-              onChange={(evt) => onChange(evt)}
-          />
+            <div className="content" style={{ padding: "16px 16px 0px 16px" }}>
+                <Typography variant="subtitle2">
+                    Hey {props.authData.user != null ? props.authData.user.username : "none"}. Welcome to your image repo. Add, update and delete images as you please.
+                </Typography>
+                <input
+                    type="file" accept='image/png'
+                    onChange={(evt) => onChange(evt)} />
+                <div style={{ display: 'flex', flexFlow: 'row wrap', marginTop: '1rem' }}>
+                    {
+                        imagesData.map((img, id) => {
+                            return <ImageCard key={id} imageData={img}/>
+                        })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
